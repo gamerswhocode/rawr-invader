5360e2516b7a2f601df7d419564684441669eb73
ferzerkerx <ferzerkerx@gmal.com>
2015-08-15 07:42:03 +0000

    initial commit

---
+++ rawr-invader/Assets/RawrApplication.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+
+class RawrApplication extends MonoBehaviour {
+	var model;
+	var view;
+	var controller_list = new Array();
+	
+	
+   function notify(event, target, data) {
+      for (var c:RawrController in controller_list) {
+         c.onNotification(event, target, data);
+      }
+   }
+   
+   function addController(controller) {
+		controller_list.push(controller);
+   }
+
+}
+
+var rawrApplication;
+function Start () {
+	rawrApplication = RawrApplication();
+}
+
+function Update () {
+
+}
+
+
---
+++ rawr-invader/Assets/RawrController.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+
+class RawrController extends MonoBehaviour {
+	
+	
+   function onNotification(event, target, data) {
+      
+   }
+}
+
+function Start () {
+	var rawrController = RawrController();
+	rawrApplication.addController(rawrController);
+}
+
+function Update () {
+
+}
